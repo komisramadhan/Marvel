86f622d96aa19d0ea60526e0b6b2ad4e4db4c4c9
komisramadhan <106125758+komisramadhan@users.noreply.github.com>
2022-06-04 06:01:25 +0700

    Add files via upload

---
+++ molekul-MgCl2 (3).cpp
@@ -0,0 +1,19 @@
+
+#include "iostream"
+#include "cmath"
+#include "iomanip"
+#include "fstream"
+
+using namespace std;
+
+int main(){
+    // deklarasi variabel
+    float pi = 3.14;
+    
+    // deklarasi molekul air
+    float Mgx, Mgy, Mgz;
+    float Cl1x, Cl1y, Cl1z;
+    float Cl2x, Cl2y, Cl2z;
+
+    // masukkan nilai untuk setiap variabel MgCl2
+    Mgx = Mgy = Mgz = 0.0;
