68641cbb528c64794a0280dab7341643866cf69a
rahmahidayani <106720003+rahmahidayani@users.noreply.github.com>
2022-06-04 09:49:05 +0700

    Update molekul-MgCl2 (3).cpp
    melanjutkan projek mengcoding molekul MgCl2

--- molekul-MgCl2 (3).cpp
+++ molekul-MgCl2 (3).cpp
@@ -17,3 +17,19 @@ int main(){
 
     // masukkan nilai untuk setiap variabel MgCl2
     Mgx = Mgy = Mgz = 0.0;
+    Cl1x = sin(88.75*pi/180) * 177.5;
+    Cl1y = 0.0;
+    Cl1z = cos(88.75*pi/180) * 177.5;
+
+    Cl2x = - Cl1x;
+    Cl2y = Cl1y;
+    Cl2z = Cl1z;
+
+    // memasukkan nilai tersebut ke dalam file
+    ofstream file;
+    file.open("MgCl2.xyz")
+    file << "3\n" << endl;
+    file << setw(3) << "Mg" << setw(3) << " " 
+    << fixed << setprecision(3) << Mgx << setw(3) << " " \
+    << fixed << setprecision(3) << Mgy << setw(3) << " " \
+    << fixed << setprecision(3) << Mgz << "\n";
