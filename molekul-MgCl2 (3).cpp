e416b30d16903e2c6371f3b63ddbf56f5971b37e
rahmahidayani <106720003+rahmahidayani@users.noreply.github.com>
2022-06-06 17:26:21 +0700

    Update molekul-MgCl2 (3).cpp
    saya menambahkan tanda < pada baris ke 40, yang awal nya (<< fixed < setprecision(3) << Cl1z << "\n";) menjadi (<< fixed << setprecision(3) << Cl1z << "\n";) . Penambahan tanda < bertujuan agar ketika projek yang dijalankan tidak eror

--- molekul-MgCl2 (3).cpp
+++ molekul-MgCl2 (3).cpp
@@ -37,7 +37,7 @@ int main(){
     file << setw(3) << "Cl1" << setw(3) << " " \
     << fixed << setprecision(3) << Cl1x << setw(3) << " " \
     << fixed << setprecision(3) << Cl1y << setw(3) << " " \
-    << fixed < setprecision(3) << Cl1z << "\n";
+    << fixed << setprecision(3) << Cl1z << "\n";
 
      file << setw(3) << "Cl2" << setw(3) << " " \
     << fixed << setprecision(3) << Cl1y << setw(3) << " " \
