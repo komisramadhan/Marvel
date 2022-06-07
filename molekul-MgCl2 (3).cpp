de71187e2630b135e2101442b5422b1eebfcca33
Fitriarahmadani <106718839+Fitriarahmadani@users.noreply.github.com>
2022-06-07 09:15:27 +0700

    Update molekul-MgCl2 (3).cpp
    saya menambahkan program untuk menghitung ep MgCl2

--- molekul-MgCl2 (3).cpp
+++ molekul-MgCl2 (3).cpp
@@ -51,3 +51,64 @@ int main(){
     return 0;
 
 }
+
+%%writefile ep-MgCl2.cpp
+
+#include "iostream"
+#include "cmath"
+#include "iomanip"
+#include "fstream"
+
+using namespace std;
+
+float densitas;          /* densitas ini memiliki satuan g cm^-3
+                            dan ini harus diinput dari user */
+// converter dari nilai cm^-3 ke A^3
+float cm3_to_A3 = 1.0E-24;  
+float numb_mol;
+float Na = 6.022E23;            // bilangan avogadro    
+float Mr_air = 95.0;            // massa relatif dari MgCl2  
+int numb_lat;                   // panjang sel simulasi    
+int N;                          // jumlah molekul 
+float volum;                    // volume dari molekul MgCl2 
+float lx, ly, lz;               // panjang sel simulasi terhitung
+float lat;                      // jarak antar molekul MgCl2 
+
+// variabel perhitungan energi potensial 
+float sigma = 2.725;            // satuan (A)
+float epsilon = 3.725;          // satuan (kj/mol)
+float rij, rcut, rcut2;
+float Ep, Ep_LJ, A12, B6;
+float rij6, rij12;
+float dx, dy, dz;       
+
+
+int main(){
+    
+    rcut = 3 * sigma;
+    rcut2 = pow(rcut,2);
+
+    cout << "program menghitung energi potensial dari MgCl2" << endl;
+    cout << "Masukkan nilai densitas dari MgCl2 (eg. 1.0): ";
+    cin >> densitas;
+
+    numb_mol = densitas * (Na / Mr_MgCl2) * cm3_to_A3;
+    cout << "Masukkan panjang sel simulasi (eg.5):";
+    cin >> numb_lat;
+    N = (pow(numb_lat,3)) * 3;
+    volum = (float)N / numb_mol;
+
+    // panjang sel simulasi secara perhitungan
+    lx = pow(volum,(1.0/3.0));
+    ly = lx;
+    lz = lx;
+    if(lx < (2*rcut)){
+        cout << "sorry pak, sel simulasi kamu terlalu besar";
+        exit(0);
+    } else{
+        cout << "panjang sel simulasi baru: " << lx << endl;
+    }
+    lat = lx / (float)numb_lat;
+
+     // deklarasi variabel
+    float pi = 3.14;
